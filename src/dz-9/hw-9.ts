acb94906dcc74f81b30336537744b8623abe507e
vivinohradova <vvinogradova922@gmail.com>
2024-01-26 11:54:07 +0200

    dz-9

---
+++ src/dz-9/hw-9.ts
@@ -0,0 +1,18 @@
+// 1
+
+function exampleFunction(param: boolean): void {}
+
+type ResultType<T> = T extends (param: infer U) => void ? U : undefined;
+
+let a: ResultType<typeof exampleFunction>;
+
+// 2
+
+type FunctionInfo<T> = T extends (param: infer P) => infer R ? [P, R] : undefined;
+
+function foo(param: boolean): string {
+    return param ? "Hello" : "World";
+}
+
+const info: FunctionInfo<typeof foo> = [true, "Hello"];
+
