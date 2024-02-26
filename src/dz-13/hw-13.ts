a11471445e66f76d31f32dbf23a4f4eb0cf013d1
Viktoriia <133902609+vivinohradova@users.noreply.github.com>
2024-02-26 13:00:19 +0200

    dz-13 retake

--- src/dz-13/hw-13.ts
+++ src/dz-13/hw-13.ts
@@ -1,210 +1,180 @@
+type Uuid = number;
+
 interface INote {
-    id: string;
+    id: Uuid;
     title: string;
-    content: string;
-    creationDate: Date;
-    editDate: Date;
+    text: string;
+    updatedAt: Date | null;
+    createdAt: Date;
     status: boolean;
-    requireConfirmation: boolean;
-
-    edit(title: string, content: string): void;
-    toggleStatus(): boolean;
+    update: (payload: INoteUpdated) => void;
+    complete: () => void;
 }
 
-interface INoteManager {
-    add(title: string, content: string, requireConfirmation?: boolean): void;
-    delete(id: string): void;
-    getById(id: string): INote | undefined;
-    getCount(): number;
-    getUndoneCount(): number;
-    search(keyword: string): INote[];
-    sortByCreationDate(): void;
+interface ISearchable {
+    search: (id: Uuid) => INote[];
 }
 
-class Note implements INote {
-    id: string;
-    title: string;
-    content: string;
-    creationDate: Date;
-    editDate: Date;
-    status: boolean;
-    requireConfirmation: boolean;
-
-    constructor(id: string, title: string, content: string, requireConfirmation: boolean) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-        this.creationDate = new Date();
-        this.editDate = new Date();
-        this.status = false;
-        this.requireConfirmation = requireConfirmation;
-    }
-
-    edit(title: string, content: string) {
-        this.title = title;
-        this.content = content;
-        this.editDate = new Date();
-        if (this.requireConfirmation) {
-            this.status = false;
-        }
-    }
-
-    toggleStatus():boolean {
-        this.status = !this.status;
-        return this.status;
-    }
+interface INoteSorting {
+    sortByStatus(): void;
+    sortByCreatedAt(): void;
 }
 
-class NoteManager implements INoteManager {
-    private notes: Note[] = [];
+type INoteUpdated = Partial<Pick<INote, 'title' | 'text'>>;
 
-    add(title: string, content: string, requireConfirmation: boolean = false) {
-        const id = Date.now().toString(); // Генерація унікального ідентифікатора
-        const newNote = new Note(id, title, content, requireConfirmation);
-        this.notes.push(newNote);
-        return id;
-    }
+interface ITodoList {
+    allCount: number;
+    incompletedCount: number;
 
-    delete(id: string) {
-        this.notes = this.notes.filter(note => note.id !== id);
-    }
+    create: (title: string, content: string) => void;
+    delete: (id: Uuid) => INote;
+    update: (id: Uuid, payload: INoteUpdated) => INote;
+    getNoteById: (id: Uuid) => INote | undefined;
+    getNotes: () => INote[];
+    complete: (id: Uuid) => void;
 
-    getById(id: string): INote | undefined {
-        return this.notes.find(note => note.id === id);
-    }
+}
 
-    getCount(): number {
+class TodoList implements ITodoList {
+    protected notes: INote[] = [];
+
+    get allCount(): number {
         return this.notes.length;
     }
 
-    getUndoneCount(): number {
-        return this.notes.filter(note => !note.status && !note.requireConfirmation).length;
-    }    
- 
-    search(keyword: string): INote[] {
+    get incompletedCount(): number {
+        return this.notes.filter(x => !x.status).length;
+    }
+
+    public create(title: string, text: string): void {
+        if (!title.trim() || !text.trim()) {
+            throw new Error("Note can't be empty!");
+        }
+        const note = new Note(title, text);
+        this.notes.push(note);
+    }
+
+    public delete(id: Uuid): INote {
+        const noteIndex = this.findByIndex(id);
+
+        const [removeNote] = this.notes.splice(noteIndex, 1);
+        return removeNote;
+    }
+    public update(id: Uuid, payload: INoteUpdated): INote {
+        const noteIndex = this.findByIndex(id);
+        const note = this.notes[noteIndex];
+        const oldNote = { ...note };
+        note.update(payload);
+        return note;
+
+    }
+    public getNoteById(id: Uuid): INote | undefined {
+        const note = this.notes[this.findByIndex(id)];
+        if (!note) {
+            throw new Error('Haven`t notes');
+        }
+        return note;
+    }
+    public getNotes(): INote[] {
+        return this.notes;
+    }
+    public complete(id: Uuid): void {
+        const noteIndex = this.findByIndex(id);
+        const note = this.notes[noteIndex];
+        note.complete();
+    }
+    private findByIndex(id: Uuid): number {
+        const noteIndex = this.notes.findIndex((x) => x.id === id);
+        if (noteIndex === -1) {
+            throw new Error(`${id} is not defined!`);
+        }
+        return noteIndex;
+    }
+
+}
+
+class TodoListWithSearch extends TodoList implements ISearchable {
+    public search(id: Uuid): INote[] {
         return this.notes.filter(note =>
-            note.title.toLowerCase().includes(keyword.toLowerCase()) ||
-            note.content.toLowerCase().includes(keyword.toLowerCase())
-        );
-    }
-
-
-    sortByCreationDate() {
-        this.notes.sort((a, b) => a.creationDate.getTime() - b.creationDate.getTime());
+            note.title.toLowerCase().includes(id.toString()) ||
+            note.text.toLowerCase().includes(id.toString()));
     }
 }
 
-class TodoListManager {
-    private noteManager: NoteManager;
-    
-    constructor() {
-        this.noteManager = new NoteManager();
+class TodoListWithSorting extends TodoList implements INoteSorting {
+    sortByStatus(): void {
+        this.notes.sort((a, b) => {
+            return a.status === b.status ? 0 : a.status ? 1 : -1;
+        });
     }
 
-    addNote(title: string, content: string, requireConfirmation: boolean = false) {
-        const id = this.noteManager.add(title, content, requireConfirmation);
-        if (!requireConfirmation) {
-            const note = this.noteManager.getById(id); 
-            if (note) {
-                note.toggleStatus();
-            }
+    sortByCreatedAt(): void {
+        this.notes.sort((a, b) => {
+            return a.createdAt.getTime() - b.createdAt.getTime();
+        });
+    }
+}
+
+abstract class BaseNote implements INote {
+    readonly id: Uuid = Math.random() * (100 - 0) + 0;
+    readonly createdAt = new Date();
+
+    updatedAt: Date | null = null;
+    status = false;
+
+    constructor(public title: string, public text: string) { }
+
+    public complete(): void {
+        this.status = true;
+    }
+
+    public abstract update(params: INoteUpdated & { [key: string]: any }): void;
+
+}
+
+class Note extends BaseNote {
+
+    public update({ title, text }: INoteUpdated): void {
+        if (title?.trim()) {
+            this.title = title;
         }
-    }
-
-    deleteNote(id: string) {
-        this.noteManager.delete(id);
-    }
-
-    getNoteById(id: string): INote | undefined {
-        return this.noteManager.getById(id);
-    }
-
-    editNote(id: string, title: string, content: string) {
-        const note = this.noteManager.getById(id);
-        if (note) {
-            note.edit(title, content);
+        if (text?.trim()) {
+            this.text = text;
         }
-    }
-
-    markNoteAsDone(id: string) {
-        const note = this.noteManager.getById(id);
-        if (note) {
-            if (!note.requireConfirmation) {
-                note.toggleStatus();
-            }
-        }
-    }
-
-    getNotesCount(): number {
-        return this.noteManager.getCount();
-    }
-
-    getUndoneNotesCount(): number {
-        return this.noteManager.getUndoneCount();
-    }
-
-    searchNotes(keyword: string): Note[] {
-        return this.noteManager.search(keyword);
-    }
-
-    sortNotesByCreationDate() {
-        this.noteManager.sortByCreationDate();
+        this.updatedAt = new Date();
     }
 }
 
 
-class TodoList {
-    private todoListManager: TodoListManager;
-
-    constructor(todoListManager: TodoListManager) {
-        this.todoListManager = todoListManager;
-    }
-
-    addNote(title: string, content: string, requireConfirmation: boolean = false) {
-        this.todoListManager.addNote(title, content, requireConfirmation);
-    }
-
-    deleteNote(id: string) {
-        this.todoListManager.deleteNote(id);
-    }
-
-    getNoteById(id: string): Note | undefined {
-        return this.todoListManager.getNoteById(id);
-    }
-
-    editNote(id: string, title: string, content: string) {
-        this.todoListManager.editNote(id, title, content);
-    }
-
-    markNoteAsDone(id: string) {
-        this.todoListManager.markNoteAsDone(id);
-    }
-
-    getNotesCount(): number {
-        return this.todoListManager.getNotesCount();
-    }
-
-    getUndoneNotesCount(): number {
-        return this.todoListManager.getUndoneNotesCount();
-    }
-
-    searchNotes(keyword: string): Note[] {
-        return this.todoListManager.searchNotes(keyword);
-    }
-
-    sortNotesByCreationDate() {
-        this.todoListManager.sortNotesByCreationDate();
+class NoteConfirmed extends BaseNote {
+    public update({ title, text }: INoteUpdated, confirm: boolean = false): void {
+        if (!confirm) return;
+        if (title?.trim()) {
+            this.title = title;
+        }
+        if (text?.trim()) {
+            this.text = text;
+        }
+        this.updatedAt = new Date();
     }
 }
 
+const todoList = new TodoList();
 
-const todoListManager: TodoListManager = new TodoListManager();
-const todoList: TodoList = new TodoList(todoListManager);
-todoList.addNote("Приклад 1", "Зміст першої нотатки");
-todoList.addNote("Приклад 2", "Зміст другої нотатки");
-todoList.editNote('1', "Приклад 1 (редаговано)", "Зміст першої нотатки (редаговано)");
-todoList.markNoteAsDone("1");
-console.log(todoList.getNotesCount());
-console.log(todoList.getUndoneNotesCount());
-console.log(todoList.searchNotes("Приклад"));
-todoList.sortNotesByCreationDate();
+todoList.create("Title1", "Text1");
+todoList.create("Title2", "Text2");
+
+// Отримуємо всі нотатки
+const notes = todoList.getNotes();
+console.log("All notes:", notes);
+
+
+// // Видаляємо другу нотатку
+const secondNoteId = notes[1].id;
+const deletedNote = todoList.delete(secondNoteId);
+console.log("Видалена нотатка:", deletedNote);
+
+
+// // Отримуємо кількість всіх та незавершених нотаток
+console.log("Загальна кількість нотаток:", todoList.allCount);
+console.log("Кількість незавершених нотаток:", todoList.incompletedCount);
